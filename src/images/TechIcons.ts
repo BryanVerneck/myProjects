243c88c7634255b1e40ea5836bb55d150dc42249
BryanVerneck <bryanverneck@gmail.com>
2023-05-24 21:56:20 -0300

    project created

---
+++ src/images/TechIcons.ts
@@ -0,0 +1,6 @@
+export const react = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg';
+export const mongodb = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg';
+export const javascript = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg';
+export const typescript = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg';
+export const nodejs = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg';
+export const tailwindcss = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,12 +8,34 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      height: {
+        "0.25": "1px",
+      },
+      colors: {
+        'charcoal': '#264653',
+        'persianGreen': '#7fa79d',
+        'saffron': '#338493',
+        'sandyBrown': '#F4A261',
+        'burntSienna': '#E76F51',
+        'white': '#efedd0',
       },
     },
+    fontFamily: {
+      'oswald': ['Oswald', 'sans-serif'],
+    },
+    minHeight: {
+			"10v": "10vh",
+			"20v": "20vh",
+			"30v": "30vh",
+			"40v": "40vh",
+			"50v": "50vh",
+			"60v": "60vh",
+			"70v": "70vh",
+			"80v": "80vh",
+			"85v": "85vh",
+			"90v": "90vh",
+			"100v": "100vh",
+		},
   },
   plugins: [],
 }
